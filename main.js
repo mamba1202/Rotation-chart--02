f89615ae8026ecf9adcb899ce9149756996bef63
mambazhang <1612786059@qq.com>
2019-02-17 11:27:09 +0800

    第二次优化

--- main.js
+++ main.js
@@ -1,26 +1,83 @@
 let n
+
 初始化()
-setInterval( ()=>{
-$(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
- .one('transitionend', (e)=>{
-     $(e.currentTarget).removeClass('leave').addClass('enter')
- })
- $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
- n +=1
+
+setInterval(()=>{
+
+  makeLeave(getImage(n))
+
+    .one('transitionend', (e)=>{
+
+      makeEnter($(e.currentTarget))
+
+    })
+
+  makeCurrent(getImage(n+1))
+
+  n += 1
+
 },3000)
 
-function x(n){
-    if(n>3){
-        n = n%3
-        if(n===3){
-            n = 3
-        }
-    }//n = 1 2 3
-    return n
+
+
+
+// 封装函数
+
+
+function getImage(n){
+
+  return $(`.images > img:nth-child(${x(n)})`)
+
 }
 
+
+
+function x(n){
+
+  if(n>3){
+
+    n = n%3
+
+    if (n===0){
+
+      n =3
+
+    }
+
+  } // n = 1 2 3
+
+  return n
+
+}
+
+
+
 function 初始化(){
-    n = 1
-    $(`.images > img:nth-child(${n})`).addClass('current')
+
+  n = 1
+
+  $(`.images > img:nth-child(${n})`).addClass('current')
+
     .siblings().addClass('enter')
+
+}
+
+
+
+function makeCurrent($node){
+
+  return $node.removeClass('enter leave').addClass('current')
+
+}
+
+function makeLeave($node){
+
+  return $node.removeClass('enter current').addClass('leave')
+
+}
+
+function makeEnter($node){
+
+  return $node.removeClass('leave current').addClass('enter')
+
 }
